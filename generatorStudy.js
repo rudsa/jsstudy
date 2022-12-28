a9b40fa2d176758d0e06c6abd27c290a8f5ea861
rudsa <rudsa@naver.com>
2022-12-28 14:52:00 +0900

    generatorStudy

--- generatorStudy.js
+++ generatorStudy.js
@@ -1,12 +1,11 @@
-var addCoffee = function(prevName, name){
-    setTimeout(function(){
-        coffeeMaker.next(prevName ? prevName + ", " + name: name)
-    })
-}
+var addCoffee = function (prevName, name) {
+  setTimeout(function () {
+    coffeeMaker.next(prevName ? prevName + ", " + name : name);
+  });
+};
 
-var coffeeGenerator = function * (){
-    var espresso = yield addCoffee("", "에스프레소");
-    console.log(espresso);
-    var americano = yield addCoffee("", "아메리카노");
-    
-}
+var coffeeGenerator = function* () {
+  var espresso = yield addCoffee("", "에스프레소");
+  console.log(espresso);
+  var americano = yield addCoffee("", "아메리카노");
+};
