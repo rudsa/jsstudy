0f928b7b78e4b6a0416faffb6d75cac17dd9de32
rudsa <rudsa@naver.com>
2022-12-28 15:05:53 +0900

    test

--- generatorStudy.js
+++ generatorStudy.js
@@ -8,4 +8,12 @@ var coffeeGenerator = function* () {
   var espresso = yield addCoffee("", "에스프레소");
   console.log(espresso);
   var americano = yield addCoffee("", "아메리카노");
+  console.log(americano);
+  var mocha = yield addCoffee("", "카페모카");
+  console.log(mocha);
+  var latte = yield addCoffee("", "카페라떼");
+  console.log(latte);
 };
+
+var coffeeMaker = coffeeGenerator();
+coffeeMaker.next();
